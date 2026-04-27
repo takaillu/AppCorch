49fd4bf60ae9f4a8ea0b281182bcd54580a04891
Takaki <takailluten@gmail.com>
2026-04-28 01:50:45 +0900

    fix: レベル表記を更新

--- js/levels.js
+++ js/levels.js
@@ -1,16 +1,19 @@
 const LEVELS = {
   beginner: {
     name: '初心者',
+    description: '5 / 10 / 20 / 30 / 50 / 80y',
     color: '#1D9E75',
     yards: [5, 10, 20, 30, 50, 80]
   },
   intermediate: {
     name: '中級者',
+    description: '5y・10y刻みミックス',
     color: '#378ADD',
     yards: [5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
   },
   advanced: {
     name: '上級者',
+    description: '5y刻み 全20種',
     color: '#D85A30',
     yards: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
   }
